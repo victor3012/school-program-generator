94eb806c3c9e60c05fa67118fefba5b11c439c75
Victor <ghastlydiscord@gmail.com>
2023-01-01 14:26:29 +0200

    requests module removed

--- server/src/main/java/me/victor/models/entities/enums/RequestStage.java
+++
@@ -1,5 +0,0 @@
-package me.victor.models.entities.enums;
-
-public enum RequestStage {
-    PENDING, ACCEPTED, DENIED
-}
--- server/src/main/java/me/victor/repositories/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package me.victor.repositories;
-
-import me.victor.models.entities.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestRepository extends JpaRepository<Request, Long> {
-}
