16a1cd03965099515b8da2604cac5a0e92ae9793
Deniz Shyukryuev <denizztto@gmail.com>
2023-01-02 15:18:37 +0200

    update input validators

---
+++ client/src/screens/School/validators.js
@@ -0,0 +1,25 @@
+import { TEACHER_ROLES, TEACHER_ROLES_NAMES } from '../../services/util';
+import * as validate from '../../services/validators';
+
+export default {
+    email: validate.globalValidators.email,
+    name: validate.globalValidators.name,
+    role: (currentRole) => {
+        return (value) => {
+            const rolePairs = Object.entries(TEACHER_ROLES_NAMES);
+            const roles = rolePairs.map(r => r[1]);
+
+            const idx = validate.inCollection(value, roles);
+
+            const roleValue = rolePairs[idx][0];
+
+            if (TEACHER_ROLES[currentRole] <= TEACHER_ROLES[roleValue]) {
+                throw new Error(`You do not have permission to assign this role`)
+            }
+        }
+    },
+    room: (value) => {
+        validate.required(value, 'Name is required');
+        validate.maxLength(value);
+    }
+}
--- client/src/services/validators.js
+++ client/src/services/validators.js
@@ -1,12 +1,13 @@
 export const stringRegExp = new RegExp(/[a-zA-Z\-\s]*/);
+const emailRegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
-export const required = (value, msg = 'Required') => {
+export const required = (value, msg = 'Field is required') => {
     if (!value) {
         throw new Error(msg);
     }
 }
 
-export const match = (value, regexp, msg = 'Invalid') => {
+export const match = (value, regexp, msg = 'Invalid value') => {
     const result = value.match(regexp);
 
     if (!result || result[0] != value) {
@@ -20,6 +21,16 @@ export const equal = (value, valueToEqual, msg = 'Fields don\'t match') => {
     }
 }
 
+export const inCollection = (value, collection, msg = 'Invalid value') => {
+    const idx = collection.indexOf(value);
+
+    if (idx === -1) {
+        throw new Error(msg);
+    }
+
+    return idx;
+}
+
 export const minLength = (value, length = 1) => {
     if (value.length < length) {
         throw new Error(`Must be at least ${length} characters long`);
@@ -29,5 +40,18 @@ export const minLength = (value, length = 1) => {
 export const maxLength = (value, length = 255) => {
     if (value.length > length) {
         throw new Error(`Cannot exceed length of ${length} characters`);
+    }
+}
+
+export const globalValidators = {
+    email: (value) => {
+        required(value, 'Email is required');
+        maxLength(value, 320);
+        match(value, emailRegExp, 'Invalid email');
+    },
+    name: (value) => {
+        required(value, 'Name is required');
+        maxLength(value, 50);
+        match(value, stringRegExp, 'Name can only contain letters');
     }
 }
