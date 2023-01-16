748e23f7c6afd45ac7aaae5b09bf44d73f5af0c5
Victor <ghastlydiscord@gmail.com>
2023-01-16 18:56:17 +0200

    optimized imports

--- server/src/main/java/me/victor/repositories/SubjectTypeRepository.java
+++ server/src/main/java/me/victor/repositories/SubjectTypeRepository.java
@@ -3,8 +3,11 @@ package me.victor.repositories;
 import me.victor.models.entities.SubjectType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubjectTypeRepository extends JpaRepository<SubjectType, Long> {
     Optional<SubjectType> findByNameAndSchoolId(String name, long id);
+
+    List<SubjectType> findBySchoolId(long id);
 }
--- server/src/main/java/me/victor/services/SubjectTypeService.java
+++ server/src/main/java/me/victor/services/SubjectTypeService.java
@@ -1,19 +1,24 @@
 package me.victor.services;
 
 import me.victor.exceptions.DataFormatException;
+import me.victor.mappers.SubjectTypeMapper;
+import me.victor.models.dto.subject.type.SubjectTypeDTO;
 import me.victor.models.entities.School;
 import me.victor.models.entities.SubjectType;
 import me.victor.repositories.SubjectTypeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class SubjectTypeService {
     private final SubjectTypeRepository repository;
+    private final SubjectTypeMapper mapper;
 
-    public SubjectTypeService(SubjectTypeRepository repository) {
+    public SubjectTypeService(SubjectTypeRepository repository, SubjectTypeMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     public Optional<SubjectType> getByName(String name, long id) {
@@ -28,5 +33,9 @@ public class SubjectTypeService {
         this.repository.save((SubjectType) new SubjectType()
                 .setSchool(school)
                 .setName(name));
+    }
+
+    public List<SubjectTypeDTO> getSubjectTypes(School school) {
+        return this.mapper.subjectTypesToDTOs(this.repository.findBySchoolId(school.getId()));
     }
 }
